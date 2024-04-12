a6c0a21ebc3b6e7e3caf1f8fd421d8ca2840d277
Stefan Markovic <stefan@janeasystems.com>
2024-04-12 23:16:32 +0200

    Security Within Security: Add Organizations and Receipts models

--- app/App_Code/Handlers/Blob.generated.cs
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -10,6 +10,8 @@
             // register blob handlers
             RegisterHandler("AttachmentBlobHandler", "\"dbo\".\"Attachments\"", "\"Attachment\"", new string[] {
                         "\"AttachmentID\""}, "Attachments Attachment", "Attachments", "Attachment");
+            RegisterHandler("ReceiptsPicture", "\"dbo\".\"Receipts\"", "\"Picture\"", new string[] {
+                        "\"ReceiptID\""}, "Receipts Picture", "Receipts", "Picture");
         }
     }
 }
